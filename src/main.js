22d6eb57de7890fd10962d7053548a54b998e863
ANNAG112 <anna.gladkiy@bk.ru>
2025-11-21 13:02:42 +0500

    logo

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
 import './style.css'
+import 'flowbite';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      freeMode: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
